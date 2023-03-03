3c2e0c036bc4382e8b48d99676bd3ed3be82ad6f
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-03-03 23:02:55 +0500

    mapping urls to view

--- store/urls.py
+++ store/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
-    path('store/',include('template.section')),
+    path('store/',include('section.urls')),
 ]
